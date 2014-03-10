fa416f44bfc271b4b9ae8b55d84228f5144cb5b1
Adam <adam@adamimac27.gateway.2wire.net>
2014-03-09 22:23:22 -0700

    Added device menu

---
+++ Dash/DRDevicesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  DRDevicesTableViewController.h
+//  Dash
+//
+//  Created by Adam Overholtzer on 3/9/14.
+//  Copyright (c) 2014 Dash Robotics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LeDiscovery.h"
+
+@interface DRDevicesTableViewController : UITableViewController <LeDiscoveryDelegate>
+
+@end
---
+++ Dash/DRDevicesTableViewController.m
@@ -0,0 +1,93 @@
+//
+//  DRDevicesTableViewController.m
+//  Dash
+//
+//  Created by Adam Overholtzer on 3/9/14.
+//  Copyright (c) 2014 Dash Robotics. All rights reserved.
+//
+
+#import "DRDevicesTableViewController.h"
+#import "LeTemperatureAlarmService.h"
+
+
+@interface DRDevicesTableViewController ()
+@property (weak, nonatomic) LeDiscovery *bleManager;
+@end
+
+@implementation DRDevicesTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.bleManager = [LeDiscovery sharedInstance];
+    self.bleManager.discoveryDelegate = self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)discoveryDidRefresh
+{
+    [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:0] withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+- (void)discoveryStatePoweredOff
+{
+    [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:0] withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return 1;
+    } else {
+        return self.bleManager.foundPeripherals.count;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"DeviceCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    if (indexPath.section == 0) {
+        cell.textLabel.text = @"UI Test";
+        cell.detailTextLabel.text = @"No robot—just play with the controls";
+    } else {
+        
+    }
+    
+    return cell;
+}
+
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+@end
