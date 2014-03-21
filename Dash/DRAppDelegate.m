9d5d7b3ffe614d954ef2b89e0bff81bc1882e9f8
Adam <adam@adamimac27.gateway.2wire.net>
2014-03-20 22:23:30 -0700

    Cleaned up app active/resign handling.

--- Dash/DRAppDelegate.m
+++ Dash/DRAppDelegate.m
@@ -32,15 +32,11 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     [[LeDiscovery sharedInstance] disconnectAllPeripherals];
-    [[LeDiscovery sharedInstance] stopScanning];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    [[LeDiscovery sharedInstance] disconnectAllPeripherals];
-    [[LeDiscovery sharedInstance] startScanningForUUIDString:kBiscuitServiceUUIDString];
-
     if ([self.window.rootViewController respondsToSelector:@selector(popToRootViewControllerAnimated:)]) {
         [(id)self.window.rootViewController popToRootViewControllerAnimated:NO];
     }
